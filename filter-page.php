212687dc6e84b722fe9241da865463e7a6e4bd27
peterirungu <peterirungu16@gmail.com>
2016-09-29 14:07:34 +0300

    fixed filter

--- filter-page.php
+++ filter-page.php
@@ -1,7 +1,66 @@
 <section>
-	<div class="container-fluid" style="background-color:#222">
-		<div class="col-lg-3 col-md-6">
-			
+	<div class="container-fluid" style="background-color:#222333">
+		<div class="row">
+		    <div class="col-lg-3">
+				<div class="form-group">
+	              <label>Select County</label>
+	              <select class="form-control">	              
+	                <option><selected>Select County Here...</selected></option>
+	                <option>County 1</option>
+	                <option>County 2</option>
+	                <option>County 3</option>
+	              </select>
+	            </div>
+            </div>
+            <div class="col-lg-3">
+				<div class="form-group">
+	              <label>Select Constituency</label>
+	              <select class="form-control">
+	                <option><selected>Select Constituency Here...</selected></option>
+	                <option>Constituency 1</option>
+	                <option>Constituency 2</option>
+	                <option>Constituency 3</option>
+	              </select>
+	            </div>
+            </div>
+            <div class="col-lg-3">
+				<div class="form-group">
+	              <label>Select Type</label>
+	              <select class="form-control">
+	                <option><selected>Select Type Here...</selected></option>
+	                <option>Boarding School</option>
+	                <option>Day Schools</option>
+	                <option>Others</option>
+	              </select>
+	            </div>
+            </div>
+            <div class="col-lg-3">
+				<div class="form-group">
+	              <label>Select Category</label>
+	              <select class="form-control">
+	                <option><selected>Select Category Here...</selected></option>
+	                <option>Private School</option>
+	                <option>Public School</option>
+	                <option>Others</option>
+	              </select>
+	            </div>
+            </div>            
 		</div>
+		<!-- end row -->
+		<!-- new row -->
+		<div class="row">
+			<div class="col-md-3 col-sm-6 col-xs-12">
+	            <div class="info-box">
+		            <span class="info-box-icon bg-grey"><i class="ion ion-ios-gear-outline"></i></span>
+		            <div class="info-box-content">
+		              <span class="info-box-text">CPU Traffic</span>
+		              <span class="info-box-number">90<small>%</small></span>
+		            </div>
+		            <!-- /.info-box-content -->
+		        </div>
+		        <!-- /.info-box -->
+            </div>
+		</div>
+		<!-- end row -->
 	</div>
 </section>
