3be11cbb0628543a2726e88be94cc85beb6e33e2
peterirungu <peterirungu16@gmail.com>
2016-09-30 11:46:56 +0300

    filter page

--- filter-page.php
+++ filter-page.php
@@ -1,7 +1,8 @@
+<?php include 'header-section.php' ?>
 <section>
 	<div class="container-fluid" style="background-color:#222333">
 		<div class="row">
-		    <div class="col-lg-3">
+			<div class="col-lg-3" style="background-color:#fff">
 				<div class="form-group">
 	              <label>Select County</label>
 	              <select class="form-control">	              
@@ -11,8 +12,7 @@
 	                <option>County 3</option>
 	              </select>
 	            </div>
-            </div>
-            <div class="col-lg-3">
+           
 				<div class="form-group">
 	              <label>Select Constituency</label>
 	              <select class="form-control">
@@ -22,20 +22,8 @@
 	                <option>Constituency 3</option>
 	              </select>
 	            </div>
-            </div>
-            <div class="col-lg-3">
-				<div class="form-group">
-	              <label>Select Type</label>
-	              <select class="form-control">
-	                <option><selected>Select Type Here...</selected></option>
-	                <option>Boarding School</option>
-	                <option>Day Schools</option>
-	                <option>Others</option>
-	              </select>
-	            </div>
-            </div>
-            <div class="col-lg-3">
-				<div class="form-group">
+
+	            <div class="form-group">
 	              <label>Select Category</label>
 	              <select class="form-control">
 	                <option><selected>Select Category Here...</selected></option>
@@ -43,24 +31,35 @@
 	                <option>Public School</option>
 	                <option>Others</option>
 	              </select>
-	            </div>
-            </div>            
-		</div>
-		<!-- end row -->
-		<!-- new row -->
-		<div class="row">
-			<div class="col-md-3 col-sm-6 col-xs-12">
-	            <div class="info-box">
-		            <span class="info-box-icon bg-grey"><i class="ion ion-ios-gear-outline"></i></span>
-		            <div class="info-box-content">
-		              <span class="info-box-text">CPU Traffic</span>
-		              <span class="info-box-number">90<small>%</small></span>
-		            </div>
-		            <!-- /.info-box-content -->
-		        </div>
-		        <!-- /.info-box -->
-            </div>
-		</div>
-		<!-- end row -->
+                </div> 
+
+	            <div class="form-group">
+                    <label>Choose Type</label>
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Boarding School
+                        </label>
+                    </div>
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Day Schools
+                        </label>
+                    </div>
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">Others
+                        </label>
+                    </div>
+                </div>				           
+			</div>
+			<!-- ./ end of the side filter panel
+			=====================================================================================
+			   content body
+			===================================================================================== -->
+			<div class="col-lg-9" style="background-color:#fff">
+		          jhtnjhthmtghmtihmtghmyihymhyhmihmy
+			  hmiohmnymninhmio
+		    </div>
+		</div>			
 	</div>
 </section>
