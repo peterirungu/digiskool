21a02012db1cadeedc442d2f19733cc112741048
purygithaiga <purygithaiga@gmail.com>
2016-10-20 15:14:28 +0300

    changed the connect file

--- admin/connect.php
+++ admin/connect.php
@@ -1,3 +1,3 @@
 <?php
-mysql_select_db('digiskool_db',  mysql_connect('localhost','root','root'))or die(mysql_error());
+mysql_select_db('digiskool_db',  mysql_connect('localhost','root',''))or die(mysql_error());
 ?>
